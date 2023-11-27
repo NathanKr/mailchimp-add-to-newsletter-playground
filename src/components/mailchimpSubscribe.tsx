66ffac2f56dcd3857e3ec37b51e97e0de2ee3055
nathankr <natankrasney@gmail.com>
2023-11-27 09:17:36 +0200

    working version also with first name and last name

--- src/components/mailchimpSubscribe.tsx
+++ src/components/mailchimpSubscribe.tsx
@@ -1,19 +1,26 @@
 import React, { useRef, useState, FormEvent, useId } from 'react';
 
 function MailchimpSubscribe() {
-  // 1. Create a reference to the input so we can fetch/clear its value.
-  const inputEl = useRef<HTMLInputElement>(null);
+  // 1. Create a reference to the input fields so we can fetch/clear their values.
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const firstNameInputRef = useRef<HTMLInputElement>(null);
+  const lastNameInputRef = useRef<HTMLInputElement>(null);
+
   // 2. Hold a message in state to handle the response from our API.
   const [message, setMessage] = useState<string>('');
   const emailInputId = useId();
+  const firstNameId = useId();
+  const lastNameId = useId();
 
   const subscribe = async (e: FormEvent) => {
     e.preventDefault();
 
-    // 3. Send a request to our API with the user's email address.
+    // 3. Send a request to our API with the user's information.
     const res = await fetch('/api/subscribe', {
       body: JSON.stringify({
-        email: inputEl.current?.value,
+        email: emailInputRef.current?.value,
+        firstName: firstNameInputRef.current?.value,
+        lastName: lastNameInputRef.current?.value,
       }),
       headers: {
         'Content-Type': 'application/json',
@@ -26,14 +33,14 @@ function MailchimpSubscribe() {
     if (error) {
       // 4. If there was an error, update the message in state.
       setMessage(error);
-
       return;
     }
 
-    // 5. Clear the input value and show a success message.
-    if (inputEl.current) {
-      inputEl.current.value = '';
-    }
+    // 5. Clear the input values and show a success message.
+    if (emailInputRef.current) emailInputRef.current.value = '';
+    if (firstNameInputRef.current) firstNameInputRef.current.value = '';
+    if (lastNameInputRef.current) lastNameInputRef.current.value = '';
+
     setMessage('Success! 🎉 You are now subscribed to the newsletter.');
   };
 
@@ -44,10 +51,30 @@ function MailchimpSubscribe() {
         id={emailInputId}
         name="email"
         placeholder="you@awesome.com"
-        ref={inputEl}
+        ref={emailInputRef}
         required
         type="email"
       />
+      
+      <label htmlFor={firstNameId}>First Name</label>
+      <input
+        id={firstNameId}
+        name="firstName"
+        placeholder="John"
+        ref={firstNameInputRef}
+        required
+        type="text"
+      />
+
+      <label htmlFor={lastNameId}>Last Name</label>
+      <input
+        id={lastNameId}
+        name="lastName"
+        placeholder="Doe"
+        ref={lastNameInputRef}
+        type="text"
+      />
+
       <div>
         {message
           ? message
---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,37 @@
+import mailchimp from "@mailchimp/mailchimp_marketing";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_API_SERVER, // e.g., us1
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, firstName, lastName } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
+
+  try {
+    await mailchimp.lists.addListMember(
+      process.env.MAILCHIMP_AUDIENCE_ID ?? "",
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      }
+    );
+
+    return res.status(201).json({ error: "" });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ error: error.message });
+    } else {
+      return res.status(500).json({ error: 'An unknown error occurred'  });
+    }
+  }
+};
